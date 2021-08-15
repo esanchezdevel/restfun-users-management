9b007a5493b3eeacfcd83cae1e1fa48a85d96e63
Enrique Sanchez <esanchez.devel@gmail.com>
2021-08-15 13:30:08 +0200

    handle authenticate errors when wrong credentials are received

--- src/main/java/online/restfun/service/AuthenticateServiceImpl.java
+++ src/main/java/online/restfun/service/AuthenticateServiceImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 import online.restfun.domain.Credential;
 import online.restfun.domain.dto.AuthenticateRequestDTO;
 import online.restfun.domain.dto.AuthenticateResponseDTO;
-import online.restfun.encryption.Encryption;
+import online.restfun.encryption.factory.EncryptionFactory;
 import online.restfun.mapping.AuthenticateMapping;
 import online.restfun.repository.CredentialRepository;
 
@@ -23,7 +23,15 @@ public class AuthenticateServiceImpl implements AuthenticateService {
 	@Override
 	public AuthenticateResponseDTO execute(AuthenticateRequestDTO request) {
 		
-		Credential credential = credentialRepository.findByKeyAndPassword(request.getKey(), Encryption.encrypt(request.getPassword(), encryptionKey));
+		Credential credential = null;
+		
+		String password = null;
+		try {
+			password = EncryptionFactory.buildEncryption().encrypt(request.getPassword(), encryptionKey);
+		} catch (Exception e) {
+			return AuthenticateMapping.buildAuthenticateResponseDTO(credential);
+		}
+		credential = credentialRepository.findByKeyAndPassword(request.getKey(), password);
 		return AuthenticateMapping.buildAuthenticateResponseDTO(credential);
 	}
 }
